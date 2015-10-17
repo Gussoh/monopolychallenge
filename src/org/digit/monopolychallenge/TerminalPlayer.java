3b5eb14ec1fd363552a0da02e6f2496138dd9fbc
Gustav Sohtell <gustav@interaktionsbyran.se>
2015-10-18 01:26:07 +0200

    no message

--- src/org/digit/monopolychallenge/TerminalPlayer.java
+++ src/org/digit/monopolychallenge/TerminalPlayer.java
@@ -1,11 +1,29 @@
 package org.digit.monopolychallenge;
 
+import java.util.Scanner;
+
 /**
  * Created by gussoh on 18/10/15.
  */
 public class TerminalPlayer extends Player {
     @Override
     void yourTurn(Game game, Board board, Tile tile) {
+        System.out.println("It is your turn.");
+        System.out.println("Board: " + board + ", tile: " + tile);
 
+        if (tile instanceof PropertyTile) {
+            System.out.println("Would you like to buy this property?");
+            Scanner s = new Scanner(System.in);
+            String yesno = s.nextLine();
+            if (yesno.equalsIgnoreCase("yes") || yesno.equalsIgnoreCase("y")) {
+                try {
+                    ((PropertyTile) tile).buy();
+                } catch (IllegalActionException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        System.out.println("No other actions impemented. End of turn.");
     }
 }
