970648d5a221b66d1498719b5df2e7f742ec0a82
Gustav Sohtell <gustav@interaktionsbyran.se>
2015-10-17 23:41:16 +0200

    asdf<

---
+++ src/org/digit/monopolychallenge/Board.java
@@ -0,0 +1,7 @@
+package org.digit.monopolychallenge;
+
+/**
+ * Created by gussoh on 17/10/15.
+ */
+public class Board {
+}
---
+++ src/org/digit/monopolychallenge/Player.java
@@ -0,0 +1,9 @@
+package org.digit.monopolychallenge;
+
+/**
+ * Created by gussoh on 17/10/15.
+ */
+public interface Player {
+
+   void youTurn(Board board);
+}
